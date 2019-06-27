9feea141e9fa0940ab6f7d79aa9453c54f94b9fe
Camille <camillemarieruiz@gmail.com>
2019-06-27 14:56:46 +0800

    added fig_script

--- fig_script.py
+++ fig_script.py
@@ -3,5 +3,12 @@
 import os
 import main_module as mm
 
-mm.run_experiment();
-mm.run_experiment("test.txt")
+
+#mm.run_experiment("No equatorials HR.txt")
+#mm.run_experiment("No equatorials exact direct_HR.txt")
+#mm.run_experiment("Timmermann no equatorials_HR.txt")
+#mm.run_experiment("Timmermann no equatorials exact direct_HR.txt")
+#mm.run_experiment("france_spain_HR.txt")
+#mm.run_experiment("Australia_HR.txt")
+#mm.run_experiment("No equatorials gt 10k_HR.txt")
+mm.run_experiment("No equatorials lt 10k_HR.txt")
