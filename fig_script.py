b1d560145f016fbbdf43dedad64acf61bfd714c2
rwalker1501 <richard.walker@epfl.ch>
2019-07-04 11:49:03 +0200

    Change to figscript only

--- fig_script.py
+++ fig_script.py
@@ -6,9 +6,9 @@ import main_module as mm
 
 #mm.run_experiment("No equatorials HR.txt")
 #mm.run_experiment("No equatorials exact direct_HR.txt")
-#mm.run_experiment("Timmermann no equatorials_HR.txt")
+mm.run_experiment("Timmermann no equatorials_HR(2).txt")
 #mm.run_experiment("Timmermann no equatorials exact direct_HR.txt")
 #mm.run_experiment("france_spain_HR.txt")
 #mm.run_experiment("Australia_HR.txt")
 #mm.run_experiment("No equatorials gt 10k_HR.txt")
-mm.run_experiment("No equatorials lt 10k_HR.txt")
+#mm.run_experiment("No equatorials lt 10k_HR.txt")
