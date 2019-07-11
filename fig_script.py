d98bd335fce03fdf57e144116cbfea721b9682c3
rwalker1501 <richard.walker@epfl.ch>
2019-07-11 16:04:37 +0200

    Changed names of experiment files in script

--- fig_script.py
+++ fig_script.py
@@ -4,11 +4,11 @@
 import main_module as mm
 
 print 'Epidemiology of culture data analysis v.1.0 Copyright (C) 2019  Richard Walker & Camille Ruiz'
-mm.run_experiment("No equatorials HR.txt")
-#mm.run_experiment("No equatorials exact direct_HR.txt")
-#mm.run_experiment("Timmermann no equatorials_HR.txt")
-#mm.run_experiment("Timmermann no equatorials exact direct_HR.txt")
-#mm.run_experiment("france_spain_HR.txt")
-#mm.run_experiment("Australia_HR.txt")
-#mm.run_experiment("No equatorials gt 10k_HR.txt")
-#mm.run_experiment("No equatorials lt 10k_HR.txt")
+mm.run_experiment("No equatorials.txt")
+mm.run_experiment("No equatorials exact direct.txt")
+mm.run_experiment("Timmermann no equatorials.txt")
+mm.run_experiment("Timmermann no equatorials exact direct.txt")
+mm.run_experiment("france_spain.txt")
+mm.run_experiment("Australia_HR.txt")
+mm.run_experiment("No equatorials gt 10k.txt")
+mm.run_experiment("No equatorials lt 10k.txt")
